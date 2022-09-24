4c4f254b2f74cf621c527b67bb3e5d5c13d857b2
Gina Mirando <gina.mirando5@gmail.com>
2022-09-24 17:57:58 -0500

    separated for MVC layout

--- src/main/java/dev/ginamirando/reachablebackend/controllers/StopService.java
+++ src/main/java/dev/ginamirando/reachablebackend/controllers/StopService.java
@@ -1,7 +1,26 @@
-//package dev.ginamirando.reachablebackend.controllers;
-//
-//public class StopService {
-//
-//    private final StopService stopService = newStopService();
-//
-//}
+package dev.ginamirando.reachablebackend.controllers;
+
+
+import dev.ginamirando.reachablebackend.models.Stop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StopService {
+
+    private final List<Stop> stops = new ArrayList<>();
+
+    public StopService() {
+        Stop stop1 = new Stop("Testing1", "W", 1, 2, false, "overthere");
+        Stop stop2 = new Stop("Testing2", "N", 2, 3, true, "somewhere");
+
+        stops.add(stop1);
+        stops.add(stop2);
+
+    }
+
+    public List<Stop>getStops() {
+        return stops;
+    }
+}
+
